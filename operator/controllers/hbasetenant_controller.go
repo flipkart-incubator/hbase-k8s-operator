20efa4f70cfda79159e51019fd3788af6819dcac
Diptam Ghosh <diptam.ghosh@BLRETV-7GFQH2MF.local>
2023-07-12 13:22:55 +0530

    added logger for configmap

--- operator/controllers/hbasetenant_controller.go
+++ operator/controllers/hbasetenant_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	context "context"
+	corev1 "k8s.io/api/core/v1"
 	time "time"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -75,6 +76,24 @@ func (r *HbaseTenantReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		log.Error(err, "Failed to get HbaseTenant")
 		return ctrl.Result{RequeueAfter: time.Second * 5}, err
 	}
+
+	configuration := &corev1.ConfigMap{}
+	configuration.Name = "hbase-config"
+	configuration.Namespace = req.Namespace
+	err = r.Client.Get(ctx, req.NamespacedName, configuration)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("HbaseTenant resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get HbaseTenant")
+		return ctrl.Result{RequeueAfter: time.Second * 5}, err
+	}
+	log.Info("ConfigMap version: ", configuration.ResourceVersion)
 
 	svc := buildService(hbasetenant.Name, hbasetenant.Name, hbasetenant.Namespace, hbasetenant.Spec.ServiceLabels, hbasetenant.Spec.ServiceSelectorLabels, []kvstorev1.HbaseClusterDeployment{hbasetenant.Spec.Datanode}, true)
 	ctrl.SetControllerReference(hbasetenant, svc, r.Scheme)
